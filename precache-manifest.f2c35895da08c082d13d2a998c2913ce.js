7a835aa58e7e2726407f602b240c1e2273dd3c6e
Ulises Santana <ulisesantana@gmail.com>
2019-06-10 19:38:50 +0100

    Updates

---
+++ precache-manifest.f2c35895da08c082d13d2a998c2913ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9841495ff192145e50043b5ac796e0b",
+    "url": "/randomizer/index.html"
+  },
+  {
+    "revision": "6ad022c46ee3911f7a57",
+    "url": "/randomizer/static/css/main.f395d050.chunk.css"
+  },
+  {
+    "revision": "1bbdcb60bcf36f7b8d61",
+    "url": "/randomizer/static/js/2.52bb06cb.chunk.js"
+  },
+  {
+    "revision": "6ad022c46ee3911f7a57",
+    "url": "/randomizer/static/js/main.c46c77b1.chunk.js"
+  },
+  {
+    "revision": "0d2e1e01d389871be794",
+    "url": "/randomizer/static/js/runtime~main.c4a993e7.js"
+  }
+]);
