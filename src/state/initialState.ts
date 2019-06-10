42bc445fab5c0010479306df98d3a94ec6d296ca
Ulises Santana <ulisesantana@gmail.com>
2019-06-10 19:55:18 +0100

    Fix typo

--- src/state/initialState.ts
+++ src/state/initialState.ts
@@ -2,12 +2,18 @@ import {categories} from './data';
 import {StoreService} from "../services";
 import {Categories} from "../types";
 import {NAMESPACE} from "../constants";
+import {apply, mapToID, sortByLabel} from "../utils";
 
 export function getInitialState(): Categories {
   const cache = StoreService.get(NAMESPACE);
   if (!!cache) {
     return cache as Categories;
   } else {
-    return categories;
+    return apply(
+      categories,
+      Object.values,
+      sortByLabel,
+      mapToID
+    );
   }
 }
